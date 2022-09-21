5d787b9c0820aee7ab511aa74bb094415f854adc
tibiSzabo <bukkfabiszti@gmail.com>
2022-09-21 10:03:20 +0200

    added padding for the whole app, added mui input & button

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+
+import { Button, TextField } from "@mui/material"
+
 import './Search.scss'
 
 function Search() {
     return (
-        <div>
+        <div className="search-container">
+            <TextField label="Movie title" variant="outlined" />
+            <Button variant="text">Search</Button>
         </div>
     )
 }
