e1b42cd19639c1aea3c9510b0c3fcdd0f661c781
tibiSzabo <bukkfabiszti@gmail.com>
2022-09-21 09:55:58 +0200

    added sass lib, fixed test exports so project can compile, created basic grid with empty components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import Search from "./components/search/Search"
+import Movie from "./components/movie/Movie"
+import Movies from "./components/movies/Movies"
+
+import { Grid } from "@mui/material"
+
 import './App.scss'
 
 function App() {
     return (
-        <div className="App">
-        </div>
+        <Grid container spacing={2}>
+            <Grid item xs={6}>
+                <Search />
+            </Grid>
+            <Grid item xs={6}>
+                <Movie />
+            </Grid>
+            <Grid item xs={6}>
+                <Movies />
+            </Grid>
+        </Grid>
     )
 }
 
--- src/components/movie/Movie.tsx
+++ src/components/movie/Movie.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Movies.scss'
+import './Movie.scss'
 
 function Movie() {
     return (
