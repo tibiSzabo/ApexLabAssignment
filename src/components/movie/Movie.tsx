b93f158630dc4938bd2531ab80ffde28183c9b20
tibiSzabo <bukkfabiszti@gmail.com>
2022-09-22 10:45:20 +0200

    implemented fail paths for wiki requests, added spinner

--- src/components/movie/Movie.tsx
+++ src/components/movie/Movie.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { MovieType } from "../../useRequest"
 
+import { CircularProgress } from "@mui/material"
+
 import './Movie.scss'
 
 type MovieProps = {
@@ -9,29 +11,80 @@ type MovieProps = {
 
 function Movie({ movie }: MovieProps) {
     const [movieWikipediaData, setMovieWikipediaData] = useState<any>(null)
+    const [isFetchingData, setIsFetchingData] = useState<boolean>(false)
+    const [noDataFound, setNoDataFound] = useState<boolean>(false)
+    const [isFetchError, setIsFetchError] = useState<boolean>(false)
 
-    const fetchMovieFromWiki = async () => {
+    const getFirstSearchResultTitle = async () => {
         const searchParams = {
             format: 'json',
             action: 'query',
-            prop: 'extracts',
-            titles: movie.name,
+            list: 'search',
+            srsearch: movie.name,
             origin: '*'
         }
         return fetch('https://en.wikipedia.org/w/api.php?' + new URLSearchParams(searchParams))
             .then((response) => response.json())
+            .then((data) => {
+                if (data.query.search.length < 1) {
+                    return null
+                }
+                return data.query.search[0].title
+            })
+            .catch((err) => {
+                setIsFetchError(true)
+                console.log(err)
+            })
+    }
+
+    const fetchMovieFromWiki = async () => {
+        setIsFetchingData(true)
+        setIsFetchError(false)
+        setNoDataFound(false)
+        setMovieWikipediaData(null)
+
+        const title = await getFirstSearchResultTitle()
+
+        if (!title) {
+            setNoDataFound(true)
+            return
+        }
+
+        const searchParams = {
+            format: 'json',
+            action: 'query',
+            prop: 'extracts',
+            titles: title,
+            origin: '*'
+        }
+        fetch('https://en.wikipedia.org/w/api.php?' + new URLSearchParams(searchParams))
+            .then((response) => {
+                setIsFetchingData(false)
+                return response.json()
+            })
+            .then((data) => {
+                const dataKey = Object.keys(data.query.pages)[0]
+                const { extract } = data.query.pages[dataKey]
+                setMovieWikipediaData(extract)
+            })
+            .catch((err) => {
+                setIsFetchError(true)
+                console.log(err)
+            })
     }
 
     useEffect(() => {
-        fetchMovieFromWiki().then((data) => {
-            const dataKey = Object.keys(data.query.pages)[0]
-            const { extract } = data.query.pages[dataKey]
-            setMovieWikipediaData(extract)
-        })
+        fetchMovieFromWiki()
     }, [movie])
 
     return (
-        <div className="movie-container" dangerouslySetInnerHTML={ { __html: movieWikipediaData }}></div>
+        <>
+            {isFetchingData && <CircularProgress className="spinner" size={80}/>}
+            { noDataFound && <div>No data found on wikipedia</div> }
+            { isFetchError && <div>Something went wrong during wikipedia search</div> }
+            { movieWikipediaData &&
+                <div className="movie-container" dangerouslySetInnerHTML={ { __html: movieWikipediaData } }></div> }
+        </>
     )
 }
 
